5fe4a69e8af9215a4cb56bbfcd725ff0769e7ae5
Honohonoho <751718620@qq.com>
2018-06-04 21:35:19 +0800

    添加 基本页面路由

---
+++ src/view/trash/Index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="trash-detail">
+        <h1>{{msg}}</h1>
+        <p>当前为笔记本{{$route.params.note_id}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            msg: "回收站笔记详情页"
+        };
+    }
+};
+</script>
+
+<style scoped>
+h1 {
+  color: blue;
+}
+</style>
